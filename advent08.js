ab8389a340705b80bddf15497098519ba1cd56fd
Douglas Lowder <dlowder@salesforce.com>
2019-12-16 12:37:56 -0800

    Day 8 part 1

---
+++ advent08.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+
+const input = fs
+  .readFileSync('./input08.txt', 'utf8')
+  .trim()
+  .split('')
+  .map(n => parseInt(n));
+
+const WIDTH = 25;
+const HEIGHT = 6;
+
+const layerSize = WIDTH * HEIGHT;
+
+let layers = [];
+let currentLayer;
+
+for (let i in input) {
+  if (i % layerSize === 0) {
+    currentLayer = [];
+    layers.push(currentLayer);
+  }
+  currentLayer.push(input[i]);
+}
+
+for (let i in layers) {
+  let sums = [0, 0, 0];
+  for (let j in layers[i]) {
+    if (layers[i][j] === 0) {
+      sums[0] = sums[0] + 1;
+    }
+    if (layers[i][j] === 1) {
+      sums[1] = sums[1] + 1;
+    }
+    if (layers[i][j] === 2) {
+      sums[2] = sums[2] + 1;
+    }
+  }
+  layers[i] = [...sums, ...layers[i]];
+}
+
+let index = 0;
+let min = 99999999999;
+
+for (let i = 0; i < layers.length; i++) {
+  if (layers[i][0] < min) {
+    index = i;
+    min = layers[i][0];
+  }
+}
+
+console.log('Part 1: ' + layers[index][1] * layers[index][2]);
